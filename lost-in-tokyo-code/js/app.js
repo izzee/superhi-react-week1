80eceb39b9fb8963c97f94052fae7d7da4af9d44
Isaac Chabon <ichabon@me.com>
2020-02-17 12:48:21 -0500

    set up intro section (lesson 14)

---
+++ lost-in-tokyo-code/js/app.js
@@ -0,0 +1,28 @@
+const Highlight = props => (
+  <span className={`relative highlight highlight-${props.color}`}>
+    <span className="relative z-2">
+      {props.children}
+    </span>
+  </span>
+)
+
+const Intro = () => (
+  <div className="m-auto-ns f4 f3-m f2-l w-80-l normal">
+    <div className="mb3 mb4-ns"><Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun places to see, play in and <Highlight color="yellow">explore</Highlight>, in <Highlight color="blue">Tokyo</Highlight>, Japan. </div>
+    <div>From <Highlight color="blue">museums</Highlight> and <Highlight color="blue">galleries</Highlight>, to <Highlight color="pink">Robot Restaurants</Highlight> and <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that keeps on giving. <Highlight color="yellow">Dattebayo!</Highlight></div>
+  </div>
+)
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      {/* Navigation Component */}
+      <Intro />
+    </div>
+    <div className="flex flex-wrap container">
+      {/* Attractions list component */}
+    </div>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'));
