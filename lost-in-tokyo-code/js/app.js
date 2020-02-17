6cedca2f4b5f242a53a0d70bdf9b3a9d6c177f3c
Isaac Chabon <ichabon@me.com>
2020-02-17 14:08:48 -0500

    set up attractions section

--- lost-in-tokyo-code/js/app.js
+++ lost-in-tokyo-code/js/app.js
@@ -1,7 +1,7 @@
-const Highlight = props => (
-  <span className={`relative highlight highlight-${props.color}`}>
+const Highlight = ({color,children}) => (
+  <span className={`relative highlight highlight-${color}`}>
     <span className="relative z-2">
-      {props.children}
+      {children}
     </span>
   </span>
 )
@@ -13,16 +13,35 @@ const Intro = () => (
   </div>
 )
 
+const NavItem = ({className, href, children, logo}) => (
+  <li className={`mh2-ns f6 f4-1 tc ${className}`}>
+    <a className="white no-underline" href={href}>
+      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children }
+    </a>
+  </li>
+)
+
 const Nav = () => (
   <nav className="pt3 pt4-ns mb4 mb0-ns">
-    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">{menu.map( item => 
-      <li className={`mh2-ns f6 f4-1 tc ${item.className}`}>
-        <a className="white no-underline" href={item.href}>{item.children}</a>
-      </li>
-    )}
+    <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
+      {menu.map( item => 
+        <NavItem key={item.children} {...item}/>
+      )}
     </ul>
   </nav>
 )
+
+const Attraction = ({ title, description, image, className }) => (
+  <div className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}>
+    <div className="relative">
+      <div className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay">
+        <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+        <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
+      </div>
+      <img src={`../images/${image}`} />
+    </div>
+  </div>
+);
 
 const App = () => (
   <div>
@@ -31,7 +50,9 @@ const App = () => (
       <Intro />
     </div>
     <div className="flex flex-wrap container">
-      {/* Attractions list component */}
+      {attractions.map(attraction => 
+        <Attraction {...attraction}/>
+      )}
     </div>
   </div>
 )
